b41d8621767b27a3d655ea02388414b376aa82a9
unknown <d341371@WA1350L24.NAEAST.AD.JPMORGANCHASE.com>
2017-04-06 14:46:14 -0400

    Update app.js to include passport & express-session, add /auth/ route, include GoogleStrategy.

--- app.js
+++ app.js
@@ -4,11 +4,24 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var session = require('express-session');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var auth = require('./routes/auth');
 
 var app = express();
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+passport.use(new GoogleStrategy({
+  clientID: '822246465440-rbv8o8b5t465fupp0voesgfipapmdio1.apps.googleusercontent.com',
+  clientSecret: '2dXAnFE6ZxBPjxhI1MdSRfeF',
+  callbackURL: 'http://localhost:3000/auth/google/callback'},
+  function(req, accessToken, refreshToken, profile, done){
+    done(null, profile);
+  }
+));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,8 +34,22 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({secret: 'anything'}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done){
+  done(null, user)
+});
+
+passport.deserializeUser(function(user, done){
+  done(null, user);
+});
+
 app.use('/', routes);
 app.use('/users', users);
+app.use('/auth', auth);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
